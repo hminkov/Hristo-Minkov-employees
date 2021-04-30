81c70ba272e231624ea0af1c7e2c89062e7f7f20
Hristo <h.minkov@abv.bg>
2021-04-30 15:30:13 +0300

    update the new way to parse diff date types

--- employees/src/main/java/main/Project.java
+++ employees/src/main/java/main/Project.java
@@ -20,38 +20,38 @@ public class Project {
 
     Project(String projectId, String dateFrom, String dateTo){
         this.projectId = projectId;
-//        this.dateFrom = convertStringToLocalDate(dateFrom.trim(), DateFormatParser.datePattern);
-        this.dateFrom = convertStringToLocalDate(dateFrom.trim());
+        this.dateFrom = convertStringToLocalDate(dateFrom.trim(), DateFormatParser.datePattern);
+//        this.dateFrom = convertStringToLocalDate(dateFrom.trim());
         setDateTo(dateTo);
     }
 
-//    private LocalDate convertStringToLocalDate(String dateString, List<String> formatStrings) {
-//        for (String formatString : formatStrings) {
-//            try {
-//                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(formatString);
-//                LocalDate localDate = LocalDate.parse(dateString, dateFormatter);
-//                return localDate;
-//            }
-//            catch (DateTimeParseException e) {}
-//        }
-//        return null;
-//    } //new way to convert all String date patterns to LocalDate
+    private LocalDate convertStringToLocalDate(String dateString, List<String> formatStrings) {
+        for (String formatString : formatStrings) {
+            try {
+                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(formatString);
+                LocalDate localDate = LocalDate.parse(dateString, dateFormatter);
+                return localDate;
+            }
+            catch (DateTimeParseException e) {}
+        }
+        return null;
+    } //new way to convert all String date patterns to LocalDate
 
-    private LocalDate convertStringToLocalDate(String date) {
-        List<Date> parse = new PrettyTimeParser().parse(date);
-        return parse
-                .get(0)
-                .toInstant()
-                .atZone(ZoneId.systemDefault())
-                .toLocalDate();
-    }
+//    private LocalDate convertStringToLocalDate(String date) {
+//        List<Date> parse = new PrettyTimeParser().parse(date);
+//        return parse
+//                .get(0)
+//                .toInstant()
+//                .atZone(ZoneId.systemDefault())
+//                .toLocalDate();
+//    }
 
     public void setDateTo(String dateTo) {
         if(dateTo.trim().equalsIgnoreCase("NULL")){
             this.dateTo = LocalDate.now();
         } else {
-//            this.dateTo = convertStringToLocalDate(dateTo.trim(), DateFormatParser.datePattern);
-            this.dateTo = convertStringToLocalDate(dateTo.trim());
+            this.dateTo = convertStringToLocalDate(dateTo.trim(), DateFormatParser.datePattern);
+//            this.dateTo = convertStringToLocalDate(dateTo.trim());
         }
     }
 }
